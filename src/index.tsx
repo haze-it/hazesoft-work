3d3b68f1cd65d7d9fde51e59aca44def43513a7e
haze-it <shoou0000@gmail.com>
2019-03-27 19:47:16 +0900

    fix hazesoft

--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,11 @@ interface Props {
   name: string;
 }
 
-const Hello: React.FC<Props> = ({ name }) => <h1>Hello, { name } !</h1>;
-
 const App: React.FC = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <React.Fragment>
-      <Hello name="react" />
       <h3>
-        Count: { count }
-        <button onClick={ () => setCount(count + 1) }>Count</button>
+        hazesoft
       </h3>
     </React.Fragment>
   );
